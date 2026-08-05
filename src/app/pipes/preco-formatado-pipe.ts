48c3f5f00232516fc2abfa5124b66e99549de596
Yasmin Ribeiro da Conceição <yasminribeiiro06@gmail.com>
2026-08-05 11:35:41 -0300

    Controle de fluxo, Pipes, Comunicação entre componentes

---
+++ src/app/pipes/preco-formatado-pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'precoFormatado',
+})
+export class PrecoFormatadoPipe implements PipeTransform {
+   transform(valor: number): string {
+    return 'R$ ' + valor.toFixed(2);
+  }
+}
+
