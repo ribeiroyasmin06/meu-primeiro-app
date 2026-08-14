62785c5330a9621f96f0fb38300169c6a11d0db1
Yasmin Ribeiro da Conceição <yasminribeiiro06@gmail.com>
2026-08-14 09:26:43 -0300

    Regresssao e Reorganizaçao

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,8 +1,5 @@
- import { Component, signal, computed, effect } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-
-
+import { Component, signal, computed, effect, inject } from '@angular/core';
+import { ProdutosService } from '../produtos.service';
 import { Produto } from '../produto/produto';
 
 @Component({
@@ -12,40 +9,8 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  //SIGNALS
-
-  //writable signal - signal (reativo) que permite alterações (com set ou update)
-
-  produtos = signal<{ nome: string; preco: number }[]>([]);
-
-  carregando = signal(true);
-
-  produtoSelecionado = signal<string | null>(null);
-
-  carrinho = signal<{ nome: string; preco: number }[]>([]);
-
-  erro = signal<string | null>(null);
-
-
-  // COMPUTED SIGNALS
-
-  // computed signal - observa outro signal e se atualiza automaticamente
-  totalProdutos = computed(() => this.produtos().length);
-
-  valorTotal = computed(() => {
-    return this.produtos().reduce((total, item) => total + item.preco, 0);
-  }); //computed signal - esse calcula o valor total dos produtos
-
-  quantidadeCarrinho = computed(() => this.carrinho().length);
-
-  totalCarrinho = computed(() => {
-    return this.carrinho().reduce((total, item) => total + item.preco, 0);
-  });
-
-  // EFFECTS
-  //método construtor - formata os objetos criados a partir desta classe
- 
- constructor(private http: HttpClient) {
+  
+  constructor() {
     // carrega da API
     this.carregarProdutos();
 
@@ -53,7 +18,6 @@ export class ListaProdutos {
     effect(() => {
       console.log('Lista de produtos alterada:', this.produtos());
     });
-
     effect(() => {
       console.log('Valor total atualizado:', this.valorTotal());
     });
@@ -64,51 +28,64 @@ export class ListaProdutos {
     });
   }
 
-    //fim do costrutor
+  private produtosService = inject(ProdutosService);
 
-    carregarProdutos() {
-    // inicia loading
-    this.carregando.set(true);
-     this.erro.set(null); // limpa erro anterior
-    this.http
-      .get<{ title: string; price: number }[]>('https://fakestoreapi.com/products')
-      .subscribe({
-        next: (dados) => {
-          // Adaptação da API para o nosso projeto
-          const produtosFormatados = dados.map((p) => ({
-            nome: p.title,
-            preco: p.price,
-          }));
+  // SIGNALS
 
-          this.produtos.set(produtosFormatados);
-          this.carregando.set(false); // finaliza loading
-        },
+  erro = signal<string | null>(null);
 
-        error: (erro) => {
-          console.error('Erro ao carregar produtos:', erro);
-          this.erro.set('Erro ao carregar produtos. Verifique sua conexão e tente novamente.');
+  carregando = signal(true);
 
-          this.carregando.set(false); // evita loading infinito
-        },
-      });
-  }
-  
-  
-  // AÇÕES QUE ALTERAM VALORES DE SIGNALS (SET E UPDATE)
+  produtos = signal<{ nome: string; preco: number }[]>([]);
+
+  produtoSelecionado = signal<string | null>(null);
+
+  carrinho = signal<{ nome: string; preco: number }[]>([]);
+
+  // COMPUTED
+
+  totalProdutos = computed(() => this.produtos().length);
+
+  valorTotal = computed(() => {
+    return this.produtos().reduce((total, item) => total + item.preco, 0);
+  });
+
+  quantidadeCarrinho = computed(() => this.carrinho().length);
+
+  totalCarrinho = computed(() => {
+    return this.carrinho().reduce((total, item) => total + item.preco, 0);
+  });
 
   exibirProduto(nome: string) {
     this.produtoSelecionado.set(nome);
   }
-  // update - adiciona um item ao writable signal
+
   adicionarProduto() {
     this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Teclado', preco: 250 }]);
   }
-  //altera um item ao writable signal - o set é pra alterar o que já existe
+
   substituirProdutos() {
     this.produtos.set([{ nome: 'Produto novo', preco: 999 }]);
   }
+
   adicionarAoCarrinho(produto: { nome: string; preco: number }) {
     this.carrinho.update((listaAtual) => [...listaAtual, produto]);
   }
-}
 
+  carregarProdutos() {
+    this.erro.set(null); // limpa erro anterior
+    this.carregando.set(true); // ativa loading
+    this.produtosService.buscarProdutos().subscribe({
+      next: (dados) => {
+        const produtos = this.produtosService.transformarProdutos(dados);
+        this.produtos.set(produtos);
+        this.carregando.set(false);
+      },
+      error: (erro) => {
+        console.error('Erro ao carregar produtos:', erro);
+        this.erro.set('Erro ao carregar produtos. Verifique sua conexão e tente novamente.');
+        this.carregando.set(false);
+      },
+    });
+  }
+}
