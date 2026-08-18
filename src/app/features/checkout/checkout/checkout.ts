4c67d55b4017fd9b3be680c7956a6d23b4722a71
Yasmin Ribeiro da Conceição <yasminribeiiro06@gmail.com>
2026-08-18 09:07:57 -0300

    Formulario de Checkout

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
   ReactiveFormsModule,
   FormGroup,
@@ -19,6 +19,8 @@ import { CarrinhoService } from '../../../core/services/carrinho.service';
 export class Checkout {
   carrinhoService = inject(CarrinhoService);
 
+  compraFinalizada = signal(false);
+
   formulario = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(3), nomeSemNumeros]),
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -26,16 +28,31 @@ export class Checkout {
   });
 
   finalizar() {
+    this.compraFinalizada.set(false);
+
+    if (this.carrinhoService.carrinhoVazio()) {
+      console.log('Não é possível finalizar uma compra com o carrinho vazio.');
+      return;
+    }
+
     if (this.formulario.invalid) {
       console.log('Formulário inválido');
+      this.formulario.markAllAsTouched();
       return;
     }
 
     const dados = this.formulario.value;
     const itens = this.carrinhoService.itens();
+    const total = this.carrinhoService.total();
 
+    console.log('Compra finalizada com sucesso!');
     console.log('Dados do formulário:', dados);
     console.log('Itens do carrinho:', itens);
+    console.log('Total da compra:', total);
+
+    this.carrinhoService.limpar();
+    this.formulario.reset();
+    this.compraFinalizada.set(true);
   }
 }
 
